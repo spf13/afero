f3be60bd43b4d5f9fc957cc3539c37f748e07aff
Jamie Wilkinson <jaq@spacepants.org>
2015-04-04 15:35:04 +1100

    Fix data race in InMemoryFile by using a sync.Mutex and locking in Read and Write methods.
    Fixes: #8.

--- memfile.go
+++ memfile.go
@@ -18,6 +18,7 @@ import (
 	"bytes"
 	"io"
 	"os"
+	"sync"
 	"sync/atomic"
 )
 
@@ -32,6 +33,7 @@ type MemDir interface {
 }
 
 type InMemoryFile struct {
+	sync.Mutex
 	at      int64
 	name    string
 	data    []byte
@@ -105,6 +107,8 @@ func (f *InMemoryFile) Read(b []byte) (n int, err error) {
 	if f.closed == true {
 		return 0, ErrFileClosed
 	}
+	f.Lock()
+	defer f.Unlock()
 	if len(f.data)-int(f.at) >= len(b) {
 		n = len(b)
 	} else {
@@ -155,6 +159,8 @@ func (f *InMemoryFile) Seek(offset int64, whence int) (int64, error) {
 func (f *InMemoryFile) Write(b []byte) (n int, err error) {
 	n = len(b)
 	cur := atomic.LoadInt64(&f.at)
+	f.Lock()
+	defer f.Unlock()
 	diff := cur - int64(len(f.data))
 	var tail []byte
 	if n+int(cur) < len(f.data) {
