696f357e5d01942aa217bfc89b8bd996961bdff7
Nithin Meppurathu <admin@nitmedia.com>
2015-12-12 20:35:18 -0500

    implement io.Writer on memFile

--- mem/file.go
+++ mem/file.go
@@ -29,6 +29,8 @@ import "time"
 const FilePathSeparator = string(filepath.Separator)
 
 type File struct {
+	io.Writer
+
 	// atomic requires 64-bit alignment for struct field access
 	at           int64
 	readDirCount int64
@@ -216,6 +218,12 @@ func (f *File) WriteAt(b []byte, off int64) (n int, err error) {
 
 func (f *File) WriteString(s string) (ret int, err error) {
 	return f.Write([]byte(s))
+}
+
+func (f *File) WriteTo(w io.Writer) (n int64, err error) {
+	ni, err := w.Write(f.data)
+	n = int64(ni)
+	return
 }
 
 func (f *File) Info() *FileInfo {
