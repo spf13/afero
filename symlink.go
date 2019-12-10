d1c75dfb6ddd659d7c7118b5bb6cb38a1b26467c
Scott Owens <sdark.arrow@gmail.com>
2019-12-10 16:24:03 +1100

    adding support for Linker and LinkReader

--- symlink.go
+++ symlink.go
@@ -13,6 +13,10 @@
 
 package afero
 
+import (
+	"errors"
+)
+
 // Symlinker is an optional interface in Afero. It is only implemented by the
 // filesystems saying so.
 // It indicates support for 3 symlink related interfaces that implement the
@@ -34,8 +38,18 @@ type Linker interface {
 	SymlinkIfPossible(oldname, newname string) error
 }
 
+// ErrNoSymlink is the error that will be wrapped in an os.LinkError if a file system
+// does not support Symlink's either directly or through its delegated filesystem.
+// As expressed by support for the Linker interface.
+var ErrNoSymlink = errors.New("symlink not supported")
+
 // LinkReader is an optional interface in Afero. It is only implemented by the
 // filesystems saying so.
 type LinkReader interface {
 	ReadlinkIfPossible(name string) (string, error)
 }
+
+// ErrNoReadlink is the error that will be wrapped in an os.Path if a file system
+// does not support the readlink operation either directly or through its delegated filesystem.
+// As expressed by support for the LinkReader interface.
+var ErrNoReadlink = errors.New("readlink not supported")
