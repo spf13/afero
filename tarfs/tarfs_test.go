65021f4c9cdc0b6054d93f226be1a8149db8c51a
L. Alberto Giménez <agimenez@sysvalve.es>
2020-09-08 21:46:07 +0200

    tarfs: use TestMain for FS setup
    We want to have the FS variable available through all the tests, so we
    we use a common "setup" function to initialise it.

--- tarfs/tarfs_test.go
+++ tarfs/tarfs_test.go
@@ -2,32 +2,39 @@ package tarfs
 
 import (
 	"archive/tar"
+	"fmt"
 	"os"
 	"testing"
 )
 
-func TestFsNew(t *testing.T) {
+var files = []struct {
+	name   string
+	exists bool
+	isdir  bool
+	size   int64
+}{
+	{"sub", true, true, 0},
+	{"sub/testDir2", true, true, 0},
+	{"testFile", true, false, 8192},
+	{"testDir1/testFile", true, false, 8192},
+
+	{"nonExisting", false, false, 0},
+}
+
+var tfs *Fs
+
+func TestMain(m *testing.M) {
 	tf, err := os.Open("testdata/t.tar")
 	if err != nil {
-		t.Fatal(err)
+		fmt.Print(err)
+		os.Exit(1)
 	}
 
-	tr := tar.NewReader(tf)
-	tfs := New(tr)
+	tfs = New(tar.NewReader(tf))
+	os.Exit(m.Run())
+}
 
-	files := []struct {
-		name   string
-		exists bool
-		isdir  bool
-		size   int64
-	}{
-		{"sub", true, true, 0},
-		{"sub/testDir2", true, true, 0},
-		{"testFile", true, false, 8192},
-		{"testDir1/testFile", true, false, 8192},
-
-		{"nonExisting", false, false, 0},
-	}
+func TestFsNew(t *testing.T) {
 
 	t.Logf("+%v", tfs)
 
