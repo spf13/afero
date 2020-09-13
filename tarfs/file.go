f6a3394b724f6d289a4a5e88c3c9fcea6ce34161
L. Alberto Giménez <agimenez@sysvalve.es>
2020-09-13 11:37:56 +0200

    test name err for windows

--- tarfs/file.go
+++ tarfs/file.go
@@ -3,6 +3,7 @@ package tarfs
 import (
 	"archive/tar"
 	"bytes"
+	"fmt"
 	"os"
 	"sort"
 	"syscall"
@@ -71,6 +72,7 @@ func (f *File) Write(p []byte) (n int, err error) { return 0, syscall.EROFS }
 func (f *File) WriteAt(p []byte, off int64) (n int, err error) { return 0, syscall.EROFS }
 
 func (f *File) Name() string {
+	fmt.Printf("f: %+v\n", f)
 	return f.h.Name
 }
 
