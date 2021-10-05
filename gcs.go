090e652a76bba9e787474d585bb5897a7c18d044
Martin Karlsch <martin@karlsch.com>
2021-10-05 18:17:13 +0200

    read GOOGLE_APPLICATION_CREDENTIALS_JSON if it is set

--- gcs.go
+++ gcs.go
@@ -37,6 +37,9 @@ type GcsFs struct {
 // You can provide additional options to be passed to the client creation, as per
 // cloud.google.com/go/storage documentation
 func NewGcsFS(ctx context.Context, opts ...option.ClientOption) (Fs, error) {
+	if json := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON"); json != "" {
+		opts = append(opts, option.WithCredentialsJSON([]byte(json)))
+	}
 	client, err := storage.NewClient(ctx, opts...)
 	if err != nil {
 		return nil, err
