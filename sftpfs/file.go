2868226784d09e88d84937fb3b78ef69ad039e2c
MRtecno98 <mr.tecno98@gmail.com>
2024-07-28 15:55:29 +0200

    add sftp file WriteAt

--- sftpfs/file.go
+++ sftpfs/file.go
@@ -100,9 +100,8 @@ func (f *File) Write(b []byte) (n int, err error) {
 	return f.fd.Write(b)
 }
 
-// TODO
 func (f *File) WriteAt(b []byte, off int64) (n int, err error) {
-	return 0, nil
+	return f.fd.WriteAt(b, off)
 }
 
 func (f *File) WriteString(s string) (ret int, err error) {
