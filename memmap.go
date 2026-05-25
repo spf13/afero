0f8bc5b6427a67425ac646ca98036c4738d7da3b
wuyangfan <yangfan.wu@succaiss.com>
2026-05-25 16:57:35 +0800

    fix: only enforce O_EXCL when O_CREATE is set
    MemMapFs rejected any open with O_EXCL when the file already existed,
    including reopens without O_CREATE. Match os/open(2) semantics where
    O_EXCL is meaningful only together with O_CREATE.
    
    Fixes spf13/afero#415

--- memmap.go
+++ memmap.go
@@ -263,7 +263,7 @@ func (m *MemMapFs) OpenFile(name string, flag int, perm os.FileMode) (File, erro
 	perm &= chmodBits
 	chmod := false
 	file, err := m.openWrite(name)
-	if err == nil && (flag&os.O_EXCL > 0) {
+	if err == nil && (flag&os.O_CREATE > 0) && (flag&os.O_EXCL > 0) {
 		return nil, &os.PathError{Op: "open", Path: name, Err: ErrFileExists}
 	}
 	if os.IsNotExist(err) && (flag&os.O_CREATE > 0) {
